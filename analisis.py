a42c96a7df3ae4cfca13e90a239b44642c3a3b32
Gabriel R <grrasskin@gmail.com>
2015-11-14 14:27:58 +0100

    borrados

--- analisis.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('eggs.csv', 'w+') as csvfile:
-    spamwriter = csv.writer(csvfile, dialect='excel')
-    spamwriter.writerow(['Spam', 'test'])
-    spamwriter.writerows([['Spam'], ['test']])
